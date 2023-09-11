f04d703eeb541e6cf3d5f9aa7a41b070a3d50310
Dair Sheishenaliev <dokadair@gmail.com>
2023-09-11 14:45:56 +0600

    Delete extra

--- apps/quotes/apps.py
+++ apps/quotes/apps.py
@@ -5,17 +5,3 @@ class QuotesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.quotes'
 
-    def ready(self):
-        from django_celery_beat.models import PeriodicTask
-        from django_celery_beat.schedulers import CrontabSchedule
-
-        crontab_schedule, _ = CrontabSchedule.objects.get_or_create(
-            hour=12,
-            minute=0,
-            timezone='Europe/Moscow',
-        )
-        PeriodicTask.objects.get_or_create(
-            name='',
-            task='apps.quotes.tasks.collect_daily_quotes',
-            crontab=crontab_schedule,
-        )
