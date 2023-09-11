fc81be504ef712d6ad21e6121e884fc0939ed66d
Dair Sheishenaliev <dokadair@gmail.com>
2023-09-11 14:45:10 +0600

    Add collect_daily_quotes to schedule

--- sibdev_test/celery.py
+++ sibdev_test/celery.py
@@ -1,5 +1,6 @@
 from celery import Celery
 from celery.signals import setup_logging
+from celery.schedules import crontab
 from django.conf import settings
 import os
 
@@ -15,6 +16,12 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
+app.conf.beat_schedule = {
+    'collect_daily_quotes': {
+        'task': 'collect_daily_quotes',
+        'schedule': crontab(minute='0', hour='12'),
+    },
+}
 
 
 @setup_logging.connect()
