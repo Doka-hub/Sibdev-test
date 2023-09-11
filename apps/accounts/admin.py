7a5f739c3422b4c00d47b9bda52329e8a7374ee2
Dair Sheishenaliev <dokadair@gmail.com>
2023-09-11 15:54:08 +0600

    Add CustomUserAdmin

---
+++ apps/accounts/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ['id', 'email']
+    ordering = ['id']
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2"),
+            },
+        ),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
